fe2c48cd1ed42ca73589199cf77956435c6183fc
pablorodrigo <pablo.silva.rodrigo@gmail.com>
2021-09-07 10:30:30 -0400

    configured Styled Components

---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,16 @@
+/**
+ * Created by Pablo Silva
+ * Date: 2021/09/07
+ * Time: 10:04
+ */
+
+import React from 'react';
+import {Container, Title} from  './styles'
+
+export function Dashboard() {
+
+    return (
+        <Container><Title>Dashboard</Title></Container>
+    );
+
+}
---
+++ src/screens/Dashboard/styles.ts
@@ -0,0 +1,20 @@
+/**
+ * Created by Pablo Silva
+ * Date: 2021/09/07
+ * Time: 10:17
+ */
+
+import styled from 'styled-components/native'
+
+export const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  background-color: #999;
+`;
+
+export const Title = styled.Text`
+    font-size: 24px;
+  font-weight: bold;
+  color: purple;
+`
