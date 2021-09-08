5b98e6d5de8d5ea9c8cc9c48c2e7b38c7dc1a0e6
pablorodrigo <pablo.silva.rodrigo@gmail.com>
2021-09-08 14:59:56 -0400

    using fonts customized, icons, proportion of measures

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -5,12 +5,37 @@
  */
 
 import React from 'react';
-import { Container, Title } from './styles';
+import {
+  Container,
+  Header,
+  UserInfo,
+  UserGreeting,
+  UserName,
+  User,
+  Photo,
+  UserContainer,
+  IconLogout,
+} from './styles';
 
 export function Dashboard() {
   return (
     <Container>
-      <Title>Dashboard</Title>
+      <Header>
+        <UserContainer>
+          <UserInfo>
+            <Photo
+              source={{
+                uri: 'https://avatars.githubusercontent.com/u/6105149?v=4',
+              }}
+            />
+            <User>
+              <UserGreeting>Hello, </UserGreeting>
+              <UserName>Pablo</UserName>
+            </User>
+          </UserInfo>
+          <IconLogout name="power" />
+        </UserContainer>
+      </Header>
     </Container>
   );
 }
