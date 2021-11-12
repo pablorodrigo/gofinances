9ad72b465f1e94c0b250bec2c043f0cf10bbf0ad
pablorodrigo <pablo.silva.rodrigo@gmail.com>
2021-11-11 23:01:32 -0400

    [ChapterII][AsyncStorage] - created list of categories and chart

--- src/screens/Resume/index.tsx
+++ src/screens/Resume/index.tsx
@@ -4,18 +4,127 @@
  * Time: 22:03
  */
 
-import React from 'react';
-import { Container, Title, Header } from './styles';
+import React, { useEffect, useState } from 'react';
+import { Container, Title, Header, Content, ChartContainer } from './styles';
 import { HistoryCar } from '../../components/HistoryCar';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import env from '../../shared/env';
+import { categories } from '../../utils/categories';
+import { VictoryPie } from 'victory-native';
+import { useTheme } from 'styled-components';
+import { RFValue } from 'react-native-responsive-fontsize';
+
+interface ITransactionProps {
+  type: 'positive' | 'negative';
+  name: string;
+  amount: string;
+  category: string;
+  date: string;
+}
+
+interface ICategoryProps {
+  key: string;
+  name: string;
+  total: number;
+  totalFormatted: string;
+  color: string;
+  percent: string;
+}
 
 export function Resume() {
+  const [totalByCategories, setTotalByCategories] = useState<ICategoryProps[]>(
+    []
+  );
+
+  const theme = useTheme();
+
+  async function loadData() {
+    const response = await AsyncStorage.getItem(env.STORAGE_DATA_KEY);
+    const responseFormatted = response ? JSON.parse(response) : [];
+
+    console.log(responseFormatted);
+
+    const outcomes = responseFormatted.filter(
+      (outcome: ITransactionProps) => outcome.type === 'negative'
+    );
+
+    const totalOutComes = outcomes.reduce(
+      (accumulator: number, outcome: ITransactionProps) => {
+        return accumulator + outcome.amount;
+      },
+      0
+    );
+
+    const totalByCategory: ICategoryProps[] = [];
+
+    categories.forEach((category) => {
+      let categorySum = 0;
+
+      outcomes.forEach((outcome: ITransactionProps) => {
+        if (outcome.category === category.key) {
+          categorySum += Number(outcome.amount);
+        }
+      });
+
+      if (categorySum > 0) {
+        const totalFormatted = categorySum.toLocaleString('pt-BR', {
+          style: 'currency',
+          currency: 'BRL',
+        });
+
+        const percent = ((categorySum / totalOutComes) * 100).toFixed(0) + '%';
+
+        totalByCategory.push({
+          key: category.key,
+          name: category.name,
+          total: categorySum,
+          color: category.color,
+          totalFormatted,
+          percent,
+        });
+      }
+    });
+
+    //console.log(totalByCategory);
+    setTotalByCategories(totalByCategory);
+  }
+
+  useEffect(() => {
+    loadData();
+  }, []);
+
   return (
     <Container>
       <Header>
         <Title>Resumo por categoria</Title>
       </Header>
 
-      <HistoryCar title="Compras" amount="150" color="red" />
+      <Content>
+        <ChartContainer>
+          <VictoryPie
+            data={totalByCategories}
+            colorScale={totalByCategories.map((category) => category.color)}
+            style={{
+              labels: {
+                fontSize: RFValue(18),
+                fontWeight: 'bold',
+                fill: theme.colors.shape,
+              },
+            }}
+            labelRadius={50}
+            x="percent"
+            y="total"
+          />
+        </ChartContainer>
+        {totalByCategories.map((item) => (
+          <HistoryCar
+            key={item.key}
+            title={item.name}
+            amount={item.totalFormatted}
+            color={item.color}
+          />
+        ))}
+      </Content>
     </Container>
   );
 }
--- src/screens/Resume/styles.ts
+++ src/screens/Resume/styles.ts
@@ -32,3 +32,12 @@ export const Title = styled.Text`
   font-size: ${RFValue(18)}px;
   font-family: ${({ theme }) => theme.fonts.regular};
 `;
+
+export const Content = styled.ScrollView.attrs({
+  contentContainerStyle: { flex: 1, padding: 24 },
+})``;
+
+export const ChartContainer = styled.View`
+  width: 100%;
+  align-items: center;
+`;
